7fa1013709c964f321c9c0b9895ff3e9f9fe04d0
nafis585 <princetoure@gmail.com>
2025-02-20 23:51:47 +0000

    resolving the problem of jdbc-connector

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/gestion_salle_sport";
+    private static final String URL = "jdbc:mysql://localhost:3306/gestion_salle_sport?allowPublicKeyRetrieval=true&useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "zack123!!!";
     private static Connection connection;
